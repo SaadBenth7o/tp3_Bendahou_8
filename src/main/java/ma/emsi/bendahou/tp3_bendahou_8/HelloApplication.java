05c68f77dc0341549beb5df85d30de65235882e3
SaadBenth7o <bendahou.saad@gmail.com>
2025-11-04 01:48:52 +0100

    first commit

---
+++ src/main/java/ma/emsi/bendahou/tp3_bendahou_8/HelloApplication.java
@@ -0,0 +1,9 @@
+package ma.emsi.bendahou.tp3_bendahou_8;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
